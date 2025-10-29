9e6179f9a59c632e5fdfd5302477d73fda709f73
Romie Agung Nugraha <romian1394@gmail.com>
2025-10-29 22:21:58 +0700

    re move dist

--- backend/dist/routes/product.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const product_controller_1 = require("@/controller/product.controller");
-const router = (0, express_1.Router)();
-router.get('/', product_controller_1.getProducts);
-exports.default = router;
-//# sourceMappingURL=product.routes.js.map
